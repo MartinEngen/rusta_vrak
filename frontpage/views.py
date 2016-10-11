3f2fc2fcf61cf6c6f52dc361c2010166372a1242
Martin Engen <martie12@student.uia.no>
2016-10-11 12:21:20 +0200

    searchv1

--- frontpage/views.py
+++ frontpage/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from cars.models import Car
+from cars.models import Car, Car_Booking
 from .forms import SearchForm
 
 
@@ -20,8 +20,6 @@ def index(request):
             print(search.cleaned_data['personal'])
             print(search.cleaned_data['van'])
             print(search.cleaned_data['combi_car'])
-
-
 
 
 
@@ -46,6 +44,47 @@ def index(request):
 def search_funciton(request):
     if request.method == 'POST':
         print('Posted info')
+
+        search_form = SearchForm(request.POST)
+
+        if search_form.is_valid():
+            print('Valid search form, lets do some queries.')
+
+            inital_date = search_form.cleaned_data['initial_date']
+            final_date = search_form.cleaned_data['final_date']
+
+            searched_types = []
+            if search_form.cleaned_data['personal']:
+                searched_types.append(1)
+            if search_form.cleaned_data['van']:
+                searched_types.append(2)
+            if search_form.cleaned_data['combi_car']:
+                searched_types.append(3)
+
+            print(searched_types)
+            # Gather all the cars with the correct car type. car__car_type__in=searched_types
+            #available_cars = Car_Booking.objects.filter(car__car_type=1)
+            cars = Car.objects.filter(car_type__in=searched_types)
+            print(cars)
+            # Exclude any cars with initial date greater than final date searched.
+            available_cars = Car_Booking.objects.filter(car=cars)
+            available_cars = available_cars.exclude(final_date__lte=final_date)
+            available_cars = available_cars.exclude(initial_date__gte=inital_date)
+
+
+            available = []
+
+            for car in available_cars:
+                print("Adding car. + " + str(car.car))
+                available.append(car.car)
+            #available_cars = available_cars.exclude(final_date__gt=inital_date)
+            #available_cars = available_cars.exclude(initial_date__lt=final_date)
+
+
+
+
+
+            print available
 
         return render(request, 'cars/search_cars.html')
 
