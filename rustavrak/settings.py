d44e2bfa02480813df63518aa9d7f97bdd9bea16
Nitrax <Martin.Engen@outlook.com>
2017-03-15 22:29:05 +0100

    Removed overriding middleware to fix langauge issues
    'django.middleware.locale.LocaleMiddleware',
    This  the little bugger making me unable to change language.
    It overrides language based on the language of the request.

--- rustavrak/settings.py
+++ rustavrak/settings.py
@@ -68,12 +68,15 @@ else:
     }
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
+LANGUAGE_CODE = 'nb'
+TIME_ZONE = 'Europe/Oslo'
+
+
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-LANGUAGE_CODE = 'nb'
-TIME_ZONE = 'Europe/Oslo'
+
 
 # Absolute path to the directory that holds media.
 # Example: "/home/username/projectname/media/"
@@ -115,7 +118,6 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -156,3 +158,20 @@ FORCE_SCRIPT_NAME = ''
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+# Password validation
+# https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
